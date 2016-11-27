9775f3edd54d457c2e174580a66918d06bded139
Renato Cardoso <renatocardoso17@gmail.com>
2016-11-27 20:23:56 +0000

    remove long description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     version=__version__,
     description='Unofficial Python API to control '
                 'Magic Blue bulbs over Bluetooth',
-    long_description='',
+    long_description='long desc',
     url='https://github.com/Betree/pyMagicBlue',
     author='Benjamin Piouffle',
     author_email='benjamin.piouffle@gmail.com',
